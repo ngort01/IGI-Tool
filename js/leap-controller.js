50a538d9079cec7ec8a64fddf381f75602d6a948
Nikolai Gorte <n.gorte@gmail.com>
2014-11-18 17:37:18 +0100

    Leap motion intergrated (read description!)
    Will only work on server now!! (e.g. XAMPP)
    
    - markers for hands
    - zoom on circle

---
+++ js/leap-controller.js
@@ -0,0 +1,179 @@
+/**
+Gestures and corresponding map events
+**/
+(function() {
+
+    LeapController.prototype.ws = null;
+
+    var MAX_ZOOM = 18;
+    var self;
+    var LEFT_HAND = 0,
+        RIGHT_HAND = 1;
+    var INDEX_FINGER = 1;
+    var handMarkers = [];
+    var HEIGHT_OFFSET = 150;
+    var BASE_MARKER_SIZE_GRIPPED = 350000,
+        BASE_MARKER_SIZE_UNGRIPPED = 500000;
+    var zoomLevelAtCircleStart;
+
+
+    function LeapController(map) {
+        self = this;
+        self.map = map;
+    }
+
+    LeapController.prototype.init = function() {
+
+        self.map.on('zoomstart', function(e) {
+            self._isZooming = true;
+        });
+
+        self.map.on('zoomend', function(e) {
+            self._isZooming = false;
+        });
+
+        //Initialize leap loop
+        var controllerOptions = {
+            enableGestures: true
+        };
+        Leap.loop(controllerOptions, function(frame) {
+			
+			// handmarkers
+            markHands(frame)
+
+            if (self._isZooming) {
+                return; // Skip this update
+            }
+
+            //Check for a hand
+            if (frame.hands != null && frame.hands.length > 0) {
+                //Check for gestures - zoom in / out
+                if (frame.gestures != null && frame.gestures.length > 0) {
+                    for (var x = 0; x < frame.gestures.length; x++) {
+                        var gesture = frame.gestures[x];
+                        if (gesture.type == "circle") {
+                            zoom(frame, gesture);
+                        }
+                    }
+                }
+            }
+        });
+    }
+
+
+
+    /**
+	Represent hands as circles on the map
+	**/
+    function markHands(frame) {
+        var scaling = (4.0 / Math.pow(2, self.map.getZoom() - 1));
+
+        var bounds = self.map.getBounds();
+        if (!bounds) {
+            return;
+        }
+		
+        var origin = L.latLng(bounds.getSouthWest().lat, bounds.getCenter().lng);
+        var hands = frame.hands;
+        //console.log(hands);
+        for (var i in hands) {
+            if (hands.hasOwnProperty(i)) {
+                // 2 hands max
+                if (i > RIGHT_HAND) {
+                    return;
+                }
+
+                var hand = hands[i];
+                newCenter = L.latLng(origin.lat + ((hand.stabilizedPalmPosition[1] - HEIGHT_OFFSET) * scaling), origin.lng 
+										+ (hand.stabilizedPalmPosition[0] * scaling));
+
+                var gripped = isGripped(hand);
+                var baseRadius = gripped ? BASE_MARKER_SIZE_GRIPPED : BASE_MARKER_SIZE_UNGRIPPED;
+
+                var handMarker = handMarkers[i];
+                if (!handMarker) {
+                    handMarker = new L.circle(newCenter, baseRadius * scaling);
+                    handMarkers[i] = handMarker;
+                }
+
+                handMarker.setLatLng(newCenter);
+                handMarker.setRadius(baseRadius * scaling);
+                handMarker.setStyle({
+					color: getHandColor(hand),
+                    opacity: 0.8,
+                    strokeWeight: 2,
+                    fill: true,
+                    fillColor: getHandColor(hand),
+                    fillOpacity: 0.35,
+                });
+                handMarker.addTo(self.map)
+            }
+        }
+    }
+
+	/**
+	Zoom in/out if circle gesture is performed clockwise/ anticlockwise
+	**/
+    function zoom(frame, circleGesture) {
+        // Only zoom based on one index finger
+        if (circleGesture.pointableIds.length == 1 &&
+            frame.pointable(circleGesture.pointableIds[0]).type == INDEX_FINGER) {
+            switch (circleGesture.state) {
+                case "start":
+                    zoomLevelAtCircleStart = self.map.getZoom();
+                    // fall through on purpose...
+                case "update":
+                    // figure out if we need to change the zoom level;
+                    var zoomChange = Math.floor(circleGesture.progress);
+                    var currentZoom = self.map.getZoom();
+                    var zoomDirection = isClockwise(frame, circleGesture) ? zoomChange : -zoomChange;
+                    if (zoomLevelAtCircleStart + zoomDirection != currentZoom) {
+                        var newZoom = zoomLevelAtCircleStart + zoomDirection;
+                        if (newZoom >= 0 && newZoom <= MAX_ZOOM) {
+                            self.map.setZoom(newZoom);
+                        }
+                    }
+                    break;
+                case "stop":
+                    zoomLevelAtCircleStart = null;
+                    break;
+            }
+        }
+    }
+
+
+	/**
+	Checks if a hand is gribbed
+	**/
+    function isGripped(hand) {
+        return hand.grabStrength == 1.0;
+    }
+
+	/**
+	Check orientation of a circle gesture
+	**/
+    function isClockwise(frame, gesture) {
+        var clockwise = false;
+        var pointableID = gesture.pointableIds[0];
+        var direction = frame.pointable(pointableID).direction;
+        var dotProduct = Leap.vec3.dot(direction, gesture.normal);
+        if (dotProduct > 0) clockwise = true;
+        return clockwise;
+    }
+
+	/**
+	Change color of the handmarker depending on its state, gripped or not
+	**/
+    function getHandColor(hand) {
+        if (isGripped(hand)) {
+            return "rgb(0,119,0)";
+        } else {
+            var tint = Math.round((1.0 - hand.grabStrength) * 119);
+            tint = "rgb(119," + tint + "," + tint + ")";
+            return tint;
+        }
+    }
+
+    window.LeapController = LeapController;
+
+}(window));
--- js/map.js
+++ js/map.js
@@ -1,49 +1,70 @@
-// Muenster coords
-var lon = 7.6286;
-var lat = 51.9629;
+(function() {
+    Map.prototype.map = null;
 
-// map definition
-var map = L.map('map', {
-    zoomControl: false,
-    center: [lat, lon],
-    zoom: 14
-})
-// relative map height			
-$("#map").height($(window).height()-160);
-map.invalidateSize();
+    function Map(id) {
+        var self = this;
+        // Muenster coords
+        var lon = 7.6286;
+        var lat = 51.9629;
 
-// gesture panel height
-$("#gestures").height($(window).height()-160);
+        // map definition
+        self.map = L.map("map", {
+                zoomControl: true,
+                center: [lat, lon],
+                zoom: 14
+            })
+			
+		// relative map height			
+		$("#map").height($(window).height() - 160);
+		self.map.invalidateSize();
 
-// OSM layer
-L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-
-// Leaflet.Geosearch: Search Bar (Provider: OpenStreetMap)
-new L.Control.GeoSearch({
-    provider: new L.GeoSearch.Provider.OpenStreetMap(),
-    showMarker: false
-}).addTo(map);
-
-// add zoom functionality
-map.addControl(L.control.zoom());
+        // OSM layer
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(self.map);
 
 
+        // Leaflet.Geosearch: Search Bar (Provider: OpenStreetMap)
+        new L.Control.GeoSearch({
+            provider: new L.GeoSearch.Provider.OpenStreetMap(),
+            showMarker: false
+        }).addTo(self.map);
 
+    }
+
+    window.Map = Map;
+
+}(window));
+
+
+
+var m = null;
+/**
+Init
+**/
+function init() {
+    m = new Map("map");
+	// gesture panel height
+    $("#gestures").height($(window).height() - 160);
+    var lc = new LeapController(m.map);
+    lc.init();
+}
+
+/**
+Load elements on document ready
+**/
 $(document).ready(function() {
-	$("#nav").ferroMenu({
-		position    : "center-bottom", // initial position of the menu in one of the 9 anchor points
-		open	: 100
-	});
-	// slim scrollbar settings
-	$('#gestures').slimScroll({
-		height: "$(window).height()-250",
-		color: '#f2f2f2',
-		railVisible: true,
-		railColor: 'white',
-		railOpacity: 0.4
-	});
+    $("#nav").ferroMenu({
+        position: "center-bottom", // initial position of the menu in one of the 9 anchor points
+        open: 100
+    });
+    // slim scrollbar settings
+    $('#gestures').slimScroll({
+        height: "$(window).height()-250",
+        color: '#f2f2f2',
+        railVisible: false,
+        railColor: 'white',
+        railOpacity: 0.4
+    });
+    init();
 });
-
