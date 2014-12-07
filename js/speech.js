16ba63d29a1a5176bb45e012d39b259ef18c4e33
Maurin Radtke <Maurin.RAdtke@gmail.com>
2014-12-07 07:52:35 +0100

    dictation modality for POI name and description
    -added speech interim user feedback
    -changed speech command user feedback
    -added dictation modality for POI name and description
    
    see:
    https://docs.google.com/document/d/144fuG_xPxVMN6y172iQj36mQLHFzwNuMWXqloDpzTUk/edit
    
    TO DO:
    implement form accessibility (?) see trello sprint comment or have a
    look at the "to-do" functions colored in red in the state flow diagram.
    also trigger function:
    stopFormRecording(); when the user canceled/closes/submits the form.

--- js/speech.js
+++ js/speech.js
@@ -2,12 +2,16 @@ var recognition = new webkitSpeechRecognition();
 var final_transcript = '';
 var interim_transcript = '';
 var language = 'en-GB'; // TODO: fetch language as option value from drop down box // en-GB
+var mapControling = true;
+var recName = false;
+var recDescription = false;
 
 recognition.continuous = true; // keep processing input until stopped
 recognition.interimResults = true; // show interim results
 recognition.lang = language; // specify the language
 
 recognition.onresult = function(event) {
+
     // Assemble the transcript from the array of results
     for (var i = event.resultIndex; i < event.results.length; ++i) {
         if (event.results[i].isFinal) {
@@ -15,15 +19,18 @@ recognition.onresult = function(event) {
         } else {
             interim_transcript = event.results[i][0].transcript;
         }
+		//console.log("#" + i + ": " + event.results[i][0].transcript);
     }
-
+	
     console.log("interim: " + interim_transcript);
     console.log("final: " + final_transcript);
-	document.getElementById("command").innerHTML =(interim_transcript);
-
+	document.getElementById("final_interim").innerHTML =(interim_transcript);
+	document.getElementById("final_command").innerHTML =("");
+	
     // update the web page
     if (final_transcript.length > 0) {
         $('#transcript').html("final:" + final_transcript);
+		
     }
 
     if (interim_transcript.length > 0) {
@@ -31,61 +38,102 @@ recognition.onresult = function(event) {
     }
 
     // handling commands
+    //////////////////////////////////////////////////////////
+    /////Start recording POI Name and Description//////////////
+    //////////////////////////////////////////////////////////
+	
+	if ((recDescription) && (final_transcript.indexOf("submit")>=0)){
+		recDescription = false;
+		// TO DO: safe and close the POI form
+		// POI.safe(); form.close();
+		
+		turnOnMapControlsPerSpeech();
+		console.log("Dictation completed.");
+		final_transcript = '';
+	}
+	
+	if ((recDescription) && ((final_transcript.length)>0)) {
+		// TO DO: add the final_transcript into the "Descirption"-field in POI form
+		// form.addDescription(final_transcript + ". ");
+		console.log("\"Description\"-Field augmented.");
+		final_transcript = '';
+	}
+	
+	if ((recName) && ((final_transcript.length)>0)) {
+		// TO DO: put the final_transcript into the "Name"-field in POI form
+		// form.setName(final_transcript);
+		recName = false;
+		// TO DO: Focus "Description"-Field in POI-Form
+		// form.FocusDescription();
+		recDescription = true;
+		console.log("\"Name\"-Field filled.");
+		final_transcript = '';
+	}
+	
 
     //////////////////////////////////////////////////////////
     /////Start using navigation controls//////////////////////
     //////////////////////////////////////////////////////////
-    if (final_transcript.indexOf("zoom in") >= 0) {
+    if ((final_transcript.indexOf("zoom in") >= 0) && (mapControling)) {
         // Code for zooming in
         m.map.zoomIn(1);
         console.log("Zoomed in");
-        final_transcript = '';
+		document.getElementById("final_command").innerHTML =("Zoomed in");
+		final_transcript = '';
     }
 
-    if (final_transcript.indexOf("zoom out") >= 0) {
+    if ((final_transcript.indexOf("zoom out") >= 0) && (mapControling)){
         // Code for zooming out
         m.map.zoomOut(1);
         console.log("Zoomed out");
-        final_transcript = '';
+		document.getElementById("final_command").innerHTML =("Zoomed out");
+		final_transcript = '';
     }
-    if ((final_transcript.indexOf("left") >= 0) 
+    if (((final_transcript.indexOf("left") >= 0) 
 		|| (final_transcript.indexOf("net") >= 0) 
-		|| (final_transcript.indexOf("next") >= 0)) {
+		|| (final_transcript.indexOf("next") >= 0)) && (mapControling)) {
         centerPoint = m.map.getCenter();
         var delta = (m.map.getBounds().getEast() - m.map.getBounds().getWest()) / 4;
         centerPoint.lng -= Math.abs(delta);
         m.map.panTo(centerPoint);
-        //console.log("Panned Left by "); // + Math.abs(m));
+        console.log("Panned Left by " + Math.abs(delta));
+		document.getElementById("final_command").innerHTML =("panned left");
         final_transcript = '';
     }
 
-    if ((final_transcript.indexOf("right") >= 0) || (final_transcript.indexOf("white") >= 0) || (final_transcript.indexOf("fight") >= 0)) {
+    if (((final_transcript.indexOf("right") >= 0) 
+		|| (final_transcript.indexOf("white") >= 0) 
+		|| (final_transcript.indexOf("fight") >= 0)) && (mapControling)) {
         centerPoint = m.map.getCenter();
         var delta = (m.map.getBounds().getEast() - m.map.getBounds().getWest()) / 4;
         centerPoint.lng += Math.abs(delta);
         m.map.panTo(centerPoint);
-        //console.log("Panned right by "); // + Math.abs(m));
+        console.log("Panned right by " + Math.abs(delta));
+		document.getElementById("final_command").innerHTML =("panned right");
         final_transcript = '';
     }
 
-    if ((final_transcript.indexOf("up") >= 0) 
+    if (((final_transcript.indexOf("up") >= 0) 
 		|| (final_transcript.indexOf("apple") >= 0)
-		|| (final_transcript.indexOf("app") >= 0)) {
+		|| (final_transcript.indexOf("app") >= 0)) && (mapControling)){
 
         centerPoint = m.map.getCenter();
         var delta = (m.map.getBounds().getNorth() - m.map.getBounds().getSouth()) / 4;
         centerPoint.lat += delta;
         m.map.panTo(centerPoint);
-        //console.log("Panned up by "); // + Math.abs(m));
+        console.log("Panned up by " + Math.abs(delta));
+		document.getElementById("final_command").innerHTML =("panned up");
         final_transcript = '';
     }
 
-    if (final_transcript.indexOf("down") >= 0) {
+    if (((final_transcript.indexOf("down") >= 0)
+		|| (final_transcript.indexOf("dawn") >= 0)) && (mapControling)){
         centerPoint = m.map.getCenter();
         var delta = (m.map.getBounds().getNorth() - m.map.getBounds().getSouth()) / 4;
         centerPoint.lat -= delta;
         m.map.panTo(centerPoint);
-        //console.log("Panned down by "); //+ Math.abs(m));
+        console.log("Panned down by " + Math.abs(delta));		
+		document.getElementById("final_command").innerHTML =("panned down");
         final_transcript = '';
     }
 }
@@ -93,4 +141,28 @@ recognition.onresult = function(event) {
 function speechButton(event) {
     final_transcript = '';
     recognition.start();
+}
+
+function turnOnMapControlsPerSpeech(){
+	mapControling = true;
+	console.log("Turning on Speech Map Controls");
+}
+
+function turnOffMapControlsPerSpeech(){
+	mapControling = false;
+	console.log("Turning off Speech Map Controls");
+}
+
+function startFormRecording(){
+	recName = true;
+	turnOffMapControlsPerSpeech();
+	console.log("\"Name\"-Field focused.");
+	final_transcript = '';
+}
+
+function stopFormRecording(){
+	recName = false;
+	recDescirption = false;
+	turnOnMapControlsPerSpeech();
+	final_transcript = '';
 }
