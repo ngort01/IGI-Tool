e838c3df5bf670738d48b3e237a175850a834732
martinkiesow <martin.kiesow@uni-muenster.de>
2015-01-30 16:56:27 +0100

    scripts to upload images, update poi

---
+++ db/updatePoi.php
@@ -0,0 +1,91 @@
+<?php 
+	/*
+	 * adds uploaded pictures to a POI
+	 */
+
+	/*
+	 * Standard single-node URI format:
+	 * mongodb://[username:password@]host:port/[database] 
+	 */
+	$uri = "mongodb://localhost:27017/igi-tool-db";
+	$options = array("connectTimeoutMS" => 30000);
+
+	/*
+	 * Include the replica set name as an option for a multi-node replica set connection:
+	 * $uri = "mongodb://myuser:mypass@host1:port1,host2:port:2/mydb";
+	 * $options = array("replicaSet" => "myReplicaSet", "connectTimeoutMS" => 30000);
+	 */
+	$client = new MongoClient($uri, $options);		// new Mongo('localhost');
+	$db = $client->selectDB("igi-tool-db");
+
+	// get tag collection
+	$collection = $db->pois;
+
+	// get request parameters
+	$poi_oid = $_REQUEST['oid'];
+	$img_oid = $_REQUEST['new_img_oid'];
+	$display_name = $_REQUEST['display_name'];
+	
+	echo 'poi_oid = ' . $poi_oid . '<br>';
+	echo 'img_oid = ' . $img_oid . '<br>';
+	echo 'display_name = ' . $display_name . '<br>';
+
+	if ($poi_oid !== '' && $img_oid !== '' && $display_name !== '') {
+	
+		$poi = $collection -> findOne( array('_id' => new MongoId($poi_oid)) );
+		$img_doc = array('name' => $display_name, 'data' => new MongoId($img_oid));
+	
+		if (count($poi['picture']['data']) == 1) {
+	
+			// create an array from the existing picture and the new one
+			echo 'Adding picture to POI...<br>Creating array from pictures...<br>';
+			
+			$collection -> update(
+				array('_id' => new MongoId($poi_oid)),
+					array('$set' =>
+						array('picture' =>
+							array(
+								$poi['picture'],	// old, existing picture
+								$img_doc			// picture to add
+							)
+						)
+					)
+				);
+	
+		} else {
+		
+			// add new picture to array or create a new array
+			echo 'Adding picture to POI...<br>';
+
+			// TODO can "array('_id' => new MongoId($poi_oid)" be replaced by $poi?
+			$collection -> update(
+			array('_id' => new MongoId($poi_oid)),
+				array( '$push' => array('picture' => $img_doc) )
+				);
+			}
+	
+			// check if picture is part of the POI
+			$cur = $collection -> find(
+					array(
+						'_id' => new MongoId($poi_oid),
+						'picture.data' => new MongoId($img_oid)
+					)
+				) -> limit(1);
+				
+			$count = $cur -> count();
+			if ($count > 0) {
+				echo 'The image "' . $display_name . '" has been added to POI "' . $poi_oid . '".<br>';
+			} else {
+				echo 'The image "' . $display_name . '" could not be added to POI "' . $poi_oid . '".<br>';
+			}
+	} else {
+		
+		echo 'Missing information. Could not add picture to POI.'; 
+	}
+
+	// Since this is an example, we'll clean up after ourselves.
+	// $collection->drop();
+
+	// Only close the connection when your app is terminating
+	$client->close();
+?>
--- dbml/uploadImg.php
+++ dbml/uploadImg.php
@@ -1,46 +1,62 @@
  <?php
 
 	$target_dir = "uploads/";
-	$target_file = $target_dir . basename($_FILES["new-img-filename"]["name"]);
+	$target_file = $target_dir . basename($_FILES["new-img-file"]["name"]);
 	$uploadOk = 1;
 	$imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
 	$poi_oid = $_REQUEST['poi_oid'];
+	$add_to_poi = $_REQUEST['add_to_poi'];
 	$display_name = $_REQUEST['display_name'];
-
+	$result_mode = $_REQUEST['result_mode'];
+	
 	// Check if image file is a actual image or fake image
 	if(isset($_POST["submit"])) {
-		$check = getimagesize($_FILES["new-img-filename"]["tmp_name"]);
+		$check = getimagesize($_FILES["new-img-file"]["tmp_name"]);
 		if($check !== false) {
-		    echo "File is an image - " . $check["mime"] . ".<br>";
+		    if ($result_mode === 'full') {
+		    	echo "File is an image - " . $check["mime"] . ".<br>";
+		    }
 		    $uploadOk = 1;
 		} else {
-		    echo "File is not an image.<br>";
+			if ($result_mode === 'full') {
+				echo "File is not an image.<br>";
+			}
 		    $uploadOk = 0;
 		}
 	}
 
 	// Check if file already exists
 	if (file_exists($target_file)) {
-		echo "Sorry, file already exists.<br>";
+		if ($result_mode === 'full') {
+			echo "Sorry, file already exists.<br>";
+		}
 		$uploadOk = 0;
 	}
 
 	// Check file size
-	if ($_FILES["new-img-filename"]["size"] > 500000) {
-		echo "Sorry, your file is too large.<br>";
+	if ($_FILES["new-img-file"]["size"] > 500000) {
+		if ($result_mode === 'full') {
+			if ($result_mode === 'full') {
+				echo "Sorry, your file is too large.<br>";
+			}
+		}
 		$uploadOk = 0;
 	}
 
 	// Allow certain file formats
 	if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
 	&& $imageFileType != "gif" ) {
-		echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
+		if ($result_mode === 'full') {
+			echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.<br>";
+		}
 		$uploadOk = 0;
 	}
 
 	// Check if $uploadOk is set to 0 by an error
 	if ($uploadOk == 0) {
-		echo "Sorry, your file was not uploaded.<br>";
+		if ($result_mode === 'full') {
+			echo "Sorry, your file was not uploaded.<br>";
+		}
 
 	// if everything is ok, try to upload file
 	} else {
@@ -54,40 +70,83 @@
 
 		// upload image
 		$grid = $db -> getGridFS();
-		$name = basename($_FILES["new-img-filename"]["name"]);
-		$id = $grid -> storeUpload('new-img-filename', $name);
+		
+		$name = basename($_FILES["new-img-file"]["name"]);
+		$id = $grid -> storeUpload('new-img-file', $name);
 
 		if ($id) {
-		    echo "The file ". $name . " has been uploaded with ID " . $id . "<br>";
+			if ($result_mode === 'full') {
+				echo 'The file "' . $name . '" has been uploaded with ID "' . $id . '".<br>';
+			} else {
+				echo $id;
+			}
 
 			// add image to poi
-			$collection = $db -> pois;
+			if ($add_to_poi === 'true') {
+				$collection = $db -> pois;
 
-			$poi = collection -> findOne( array('_id' => new MongoId($poi_oid)) );
-			$img_doc = array('name' => $display_name, 'data' => new MongoId($id));
+				$poi = $collection -> findOne( array('_id' => new MongoId($poi_oid)) );
+				$img_doc = array('name' => $display_name, 'data' => new MongoId($id));
+				
+				// if (is_array($poi['picture']) || is_null($poi['picture'])) {
+				// didn't work, because in php documents and arrays both are arrays
+				// workaround:
+				// if picture has got a data attribute, it is a single picture entry
+				// not an array of picture entries
+				if (count($poi['picture']['data']) == 1) {
 
-			if (is_array($poi['picture']) || is_null($poi['picture'])) {
-
-				// add new picture to array
-				// TODO can "array('_id' => new MongoId($poi_oid)" be replaced by $poi?
-				$collection -> update( array('_id' => new MongoId($poi_oid),
-							array( '$push' => array('picture' => $img_doc) )
-						)
-					);
-			} else {
-
-				// create an array from the existing picture and the new one
-				$collection -> update( array('_id' => new MongoId($poi_oid),
-							array('$set' => array('picture' => array( 
-								$poi['picture'],	// old, existing picture
-								$img_doc ))			// picture to add
+					// create an array from the existing picture and the new one
+					if ($result_mode === 'full') {
+						echo 'Adding picture to POI...<br>Creating array from pictures...<br>';
+					}
+					$collection -> update(
+							array('_id' => new MongoId($poi_oid)),
+							array('$set' =>
+									array('picture' =>
+											array(
+													$poi['picture'],	// old, existing picture
+													$img_doc			// picture to add
+											)
+									)
 							)
-						)
 					);
+
+				} else {
+					
+					// add new picture to array or create a new array
+					if ($result_mode === 'full') {
+						echo 'Adding picture to POI...<br>';
+					}
+					// TODO can "array('_id' => new MongoId($poi_oid)" be replaced by $poi?
+					$collection -> update(
+							array('_id' => new MongoId($poi_oid)),
+							array( '$push' => array('picture' => $img_doc) )
+						);
+				}
+				
+				// check if picture is part of the POI
+				if ($result_mode === 'full') {
+					
+					$cur = $collection -> find(
+							array(
+								'_id' => new MongoId($poi_oid),
+								'picture.data' => new MongoId($id)
+							)
+						) -> limit(1);
+					
+					$count = $cur -> count();
+					if ($count > 0) {
+						echo 'The image "' . $display_name . '" has been added to POI "' . $poi_id . '".<br>';
+					} else {
+						echo 'The image "' . $display_name . '" could not be added to POI "' . $poi_id . '".<br>';
+					}
+				}
 			}
 
 		} else {
-		    echo "Sorry, there was an error uploading your file.<br>";
+			if ($result_mode === 'full') {
+				echo "Sorry, there was an error uploading your file.<br>";
+			}
 		}
 	}
-?> 
+?>
